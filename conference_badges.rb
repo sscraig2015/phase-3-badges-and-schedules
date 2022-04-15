9b4b9d5ea037fd7ed71ea00b38799f325ba91269
Sean <craig.sean92@gmail.com>
2022-04-14 22:11:21 -0600

    commit tree 6aaa1561756a00deb22026e1c977739d341a9529

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array_of_badge = []
+    array.each{|name| array_of_badge << badge_maker(name)}
+    return array_of_badge
+end
+
+def assign_rooms(array)
+    final_array = []
+    array.each_with_index {|name, index| final_array << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+    return final_array
+end
+
+def printer(array)
+
+    array.each{|name| print badge_maker(name)}
+    
+
+end
